dae1c4b287022f3caa376f6b68b2bde3f9ae9944
nabu0305 <nabu010305@gmail.com>
2023-04-15 07:54:10 +0000

    인강전 조건문을 이용하여 2 이상 9이하 값만 받도록하기

--- src/gugudan.java
+++ src/gugudan.java
@@ -5,10 +5,15 @@ public class gugudan {
         System.out.println("구구단 중 출력할 단은? : ");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
+        if(number > 9 || number < 2){
+            System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
 
-        for(int i=1;i<=9;i++){
-            int result = number * i;     
-            System.out.println("number : " + result);
-        }    
+        }
+        else{
+            for(int i=1;i<=9;i++){
+                int result = number * i;     
+                System.out.println("number : " + result);
+            }
+        } 
     }
 }
