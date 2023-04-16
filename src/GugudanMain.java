28723624f35bfedf98f13ac7023001accc62f7a9
nabu0305 <nabu010305@gmail.com>
2023-04-16 12:37:16 +0000

    캘린더 println으로 작성해보기

--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class GugudanMain{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String inputValue = scanner.nextLine();
-        String[] splitedValue = inputValue.split(",");
-        int first = Integer.parseInt(splitedValue[0]);
-        int second = Integer.parseInt(splitedValue[1]);
+        String LineValue = scanner.nextLine();
+        String[] SplitValue = LineValue.split(",");
+        int first = Integer.parseInt(SplitValue[0]);
+        int second = Integer.parseInt(SplitValue[1]);
         for(int i=2;i<=first;i++){
-            int[]result = gugudan.calculate(i,second);
+            int[] result = gugudan.calculate(i,second);
             gugudan.print(result);
-        }     
+        }
     }
 }
