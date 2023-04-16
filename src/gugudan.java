25cda0b875b0e64f79f27f72d00ed4147a282e01
nabu0305 <nabu010305@gmail.com>
2023-04-16 04:59:58 +0000

    인강보기전 배열을 이용하여 구구단

--- src/gugudan.java
+++ src/gugudan.java
@@ -2,17 +2,11 @@ import java.util.Scanner;
 
 public class gugudan {
     public static void main(String[] args) {
-        System.out.println("구구단 중 출력할 단은? : ");
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        if(number > 9 || number < 2){
-            System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
-
-        }
-        else{
-            for(int i=1;i<=9;i++){
-                int result = number * i;     
-                System.out.println("number : " + result);
+        int[] result = new int[10];
+        for(int j=2; j < 10; j++) {
+            for(int k=1;k<10;k++){
+                result[k] = j*k;
+                System.out.println(result[k]);
             }
         } 
     }
