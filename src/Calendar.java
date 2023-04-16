70586fb499bd14cd59e40b8235abac875da0c62d
nabu0305 <nabu010305@gmail.com>
2023-04-16 13:04:38 +0000

    반복으로 월을 입력받기

--- src/Calendar.java
+++ src/Calendar.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 public class Calendar{
     public static void main(String[] args) {
         int[] Days = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-        System.out.println("달을 입력하세요.");
         Scanner scanner = new Scanner(System.in);
-        int month = scanner.nextInt();
-        System.out.println(month + "월은" + Days[month] + "일까지 있습니다.");
+        System.out.println("반복횟수를 입력하세요.");
+        int num = scanner.nextInt();
+        for(int i=0;i<num;i++){
+            System.out.println("월을 입력하세요.");
+            int month = scanner.nextInt();
+            System.out.println(month + "월은" + Days[month] + "일까지 있습니다.");
+        }
     }
 }
