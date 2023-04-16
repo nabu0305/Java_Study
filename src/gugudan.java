985e83ccdedf034dd2aa39946823066d22a365e7
nabu0305 <nabu010305@gmail.com>
2023-04-16 05:45:59 +0000

    메서드를 이용하여 중복되는 부분 줄이기

--- src/gugudan.java
+++ src/gugudan.java
@@ -1,13 +1,28 @@
 import java.util.Scanner;
 
 public class gugudan {
+    public static int[] calculate(int times) {
+        int[] result = new int[9];
+        for (int i = 0; i < result.length; i++) {
+            // 계산 결과를 배열에 저장
+            result[i] = times * (i+1); 
+        }
+
+        return result;
+    }
+
+    public static void print(int[] result) {
+        for (int i = 0; i < result.length; i++) {
+            // 계산 결과를 화면에 출력
+            System.out.println(result[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        int[] result = new int[10];
-        for(int j=2; j < 10; j++) {
-            for(int k=1;k<10;k++){
-                result[k] = j*k;
-                System.out.println(result[k]);
-            }
-        } 
+        for(int i=2;i<10;i++){
+            int[]result = calculate(i);
+            print(result);
+        }
+        
     }
 }
