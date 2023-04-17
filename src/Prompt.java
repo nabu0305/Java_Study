027aed55f79a716658028de5caa5dd5bbc3b600b
nabu0305 <nabu010305@gmail.com>
2023-04-17 11:56:21 +0000

    윤년 구현하기

--- src/Prompt.java
+++ src/Prompt.java
@@ -4,19 +4,25 @@ public class Prompt{
     private final static String PROMPT = "cal> ";
     
     public void runPrompt(){
-        int[] Days = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+        
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();
+        
+        int month = 1;
+        int year = 2017;
+        
         while(true){
+            System.out.println("년도를 입력하세요.");
+            year = scanner.nextInt();
             System.out.println("월을 입력하세요.");
             System.out.print(PROMPT);
-            int month = scanner.nextInt();
+            month = scanner.nextInt();
             if(month == -1){
                 break;
             }
             if(month > 12)continue;
             
-            cal.printCalendar(2017,month);
+            cal.printCalendar(year,month);
         }
         System.out.println("Have a nice day");
         scanner.close();
